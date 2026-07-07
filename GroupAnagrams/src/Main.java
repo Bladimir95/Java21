0e1d25d4b5f017b73a417747c11cb7dd795e393e
Bladimir95 <i.bladimir.ac@gmail.com>
2026-07-06 19:23:23 -0600

    feat: Group of anagrams

--- GroupAnagrams/src/Main.java
+++ GroupAnagrams/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> palabras = new ArrayList<>(Arrays.asList(
+                "eat",
+                "tea",
+                "ate",
+                "tan",
+                "nat",
+                "bat",
+                "tab",
+                "listen",
+                "silent",
+                "enlist",
+                "google",
+                "gogole",
+                "abc",
+                "cab",
+                "bca",
+                "xyz",
+                "zyx",
+                "hello",
+                "olleh",
+                "java",
+                "avaj",
+                "spring",
+                "grinsp",
+                "code",
+                "deco",
+                "dog",
+                "god",
+                "cat"
+        ));
+
+        System.out.println(Group.groupOfAnagrams(palabras));
     }
 }
